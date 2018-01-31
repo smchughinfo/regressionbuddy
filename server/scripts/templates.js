eb22c22e1d403cf5c22b886832f17b83e1ca6fc6
Sean McHugh <seanmchugh.info@gmail.com>
2018-01-30 19:45:25 -0500

    li_with_sublist template

--- server/scripts/templates.js
+++ server/scripts/templates.js
@@ -1,26 +1,43 @@
+// cheerio can't find elements inside of a <template> tag. so <template> is called <placeholder>
+
+const { readFileSync } = require("fs");
 const cheerio = require('cheerio');
 
 const applyTemplates = html => {
     $ = cheerio.load(html);
-    let templates = $.root().find("template");
+    let templates = $.root().find("placeholder");
     templates.each(applyTemplate);
     return $.html();
 };
 
 const applyTemplate = (i, elm) => {
-    let template = $(elm);
-    let templateParts = template.html().split("\n");
-
-    let newlineIndex = templateParts.indexOf("\n");
-    console.log(templateParts)
-    let templateName = templateParts.splice(0, newlineIndex);
-    let items = templateParts.splice(templateParts);
-
-    console.log(templateName);
-
-   // console.log(items);
-    //let templateName = `${process.env.templatesDir}/${templateParts[0]}`;
+    let $placeholder = $(elm);
+    let templateType = $placeholder.attr("type");
+    let functionName = `apply_${templateType}`;
+    templates[functionName]($placeholder);
 }
+
+let templates = {
+    apply_li_with_sublist: $placeholder => {
+        let templatePath = `${process.env.templatesDir}/li_with_sublist.html`;
+        let $template = $(readFileSync(templatePath).toString());
+            
+        let $items = $placeholder.find("item");
+        let $repeater = $template.find("[repeater]");
+        let $repeatContainer = $repeater.parent();
+
+        $repeater.remove();
+        $repeater.removeAttr("repeater");
+        
+        $items.each((i, elm) => {
+            let $repeaterClone = $repeater.clone();
+            $repeaterClone.html($(elm).html());
+            $repeatContainer.append($repeaterClone);
+        });
+        
+        $placeholder.replaceWith($template);
+    }
+};
 
 module.exports = {
     applyTemplates: applyTemplates
