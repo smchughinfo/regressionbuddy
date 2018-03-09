387ac6b2754f7d385312ec3c1aaebb96dd5c62f4
Sean McHugh <seanmchugh.info@gmail.com>
2018-03-08 21:01:03 -0500

    i guess finish trigonometry problems for post 2

--- client/scripts/master.js
+++ client/scripts/master.js
@@ -31,7 +31,7 @@ window.addEventListener("load", function () {
         loaded = true;
     }
     MathJax.Hub.Queue(loadImages);
-    setTimeout(loadImages, 2000);
+    setTimeout(loadImages, 500);
 });
 
 /****** MATHJAX ******/
