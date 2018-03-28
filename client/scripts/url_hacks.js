ab12bf679320bf7bc27d88aa315d14d362723ced
Sean McHugh <seanmchugh.info@gmail.com>
2018-03-28 00:12:22 -0400

    post 3 algebra appendix first try

--- client/scripts/url_hacks.js
+++ client/scripts/url_hacks.js
@@ -4,14 +4,14 @@
     a.href = window.location.href;
     if (a.pathname === "" || a.pathname === "/") {
         console.log("Changing title here. Trying to get it to show up right on google but still make sense to users");
-        // TOOO: maybe - google "regression buddy" and site title is "Algebra: Week 2" except for the "index" page it should be more general
+        // TOOO: maybe - google "regression buddy" and site title is "Algebra: Post 2" except for the "index" page it should be more general
         // serve it with a general title and then change it here. hope that works.
 
         try {
             var postNumber = getPostNumber();
             var subject = getSubject();
             var redirectUrl = window.location.href + postNumber + "/" + subject;
-            document.title = "Week " + postNumber + " - " + capatalizeFirstLetterOfEveryWord(subject);
+            document.title = "Post " + postNumber + " - " + capatalizeFirstLetterOfEveryWord(subject);
 
             console.log("this needs to run before getting disqus comment count");
             history.replaceState(null, document.title, redirectUrl);
