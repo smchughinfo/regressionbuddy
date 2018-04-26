beb314492052ecca6a858ca90de44edf3ce1cab0
seanmchughinfo <seanmchugh.info@gmail.com>
2018-04-26 16:38:46 -0400

    take out unnecessary warning about mathjax version

--- server/scripts/builder.js
+++ server/scripts/builder.js
@@ -121,11 +121,6 @@ const buildMasterPage = () => {
     masterPage = masterPage.replace("[BOOTSTRAP CSS]", `<link rel="stylesheet" href="${isDev() ? "/Bootstrap/css/bootstrap.min.css" : "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"}">`);
     masterPage = masterPage.replace("[MATHJAX JS]", `<script src='${isDev() ? "/MathJax/MathJax.js?config=TeX-MML-AM_CHTML" : "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML"}'></script>`);
 
-    if(isDev()) {
-        console.log("WARNING: hosting mathjax locally. if problems, use the cdn or make sure the mathjax repository in /public/MathJax is set to version 2.7.2: \n cd regressionbuddy/client/public/MathJax \n git checkout 2.7.2".yellow);
-        // yes, there are differences if you use latest (which at this time is 2.7.4). one thing is the brackets on matrices don't go to the bottom row.
-    }
-
     masterPage = replacePlaceholderWithHTMLFile(masterPage);
     
     writeFileSync(siteHTMLPath, masterPage);
